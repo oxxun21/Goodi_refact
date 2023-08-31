68aa65b5f1f688b5ff631a2a795b85949aa8e74b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-08-31 17:58:15 +0900

    refactor : 좋아요 api 수정

--- src/api/like.js
+++ src/api/like.js
@@ -3,17 +3,21 @@ import { instance } from "./instance";
 export const likeAPI = async (id) => {
   try {
     const response = await instance.post(`/post/${id}/heart`);
-    return response.data.post.heartCount;
+    console.log(response);
+    return response.data;
   } catch (error) {
-    throw error;
+    console.log(error);
+    return error;
   }
 };
 
 export const cancelLikeAPI = async (id) => {
   try {
     const response = await instance.delete(`/post/${id}/unheart`);
-    return response.data.post.heartCount;
+    console.log(response);
+    return response.data;
   } catch (error) {
-    throw error;
+    console.log(error);
+    return error;
   }
 };
