e1b68fb35a3f1330ca91aaaee11ca444bfc20a63
Oh Ga Eun <dhrkdms4113@naver.com>
2023-09-08 00:56:20 +0900

    docs : 이미지 최적화 함수 index.js 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,6 @@ import checkImageUrl from "./checkImageUrl";
 import { setLoginCookie, getLoginCookie, removeLoginCookie } from "./loginCookie";
 import BASE_URL from "./BASE_URL";
 import priceDivide from "./priceDivide";
+import { handleImageCompression } from "./imageCompression";
 
-export { checkImageUrl, setLoginCookie, getLoginCookie, removeLoginCookie, BASE_URL, priceDivide };
+export { checkImageUrl, setLoginCookie, getLoginCookie, removeLoginCookie, BASE_URL, priceDivide, handleImageCompression };
