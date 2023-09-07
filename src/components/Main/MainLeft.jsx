a8908c6b477c215c10ab8a5387f5ae02020f8c3d
Oh Ga Eun <dhrkdms4113@naver.com>
2023-09-07 19:51:23 +0900

    fix : MainLeft 팔로잉한 유저가 없는 경우

--- src/components/Main/MainLeft.jsx
+++ src/components/Main/MainLeft.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-
-import commonBanner from "../../assets/common-banner.svg";
-import { checkImageUrl } from "../../utils";
 import { Link } from "react-router-dom";
+
+import cartNullIcon from "../../assets/profile_img_def.svg";
+import commonBanner from "../../assets/common-banner.svg";
+
+import { checkImageUrl } from "../../utils";
 
 export default function MainLeft({ render }) {
   return (
@@ -13,25 +15,62 @@ export default function MainLeft({ render }) {
       </h2>
 
       <article>
-        {render
-          .filter((i) => i !== undefined)
-          .map((item, index) => (
-            <Link key={item.id} to={`/productDetail/${item.id}`}>
-              <img src={checkImageUrl(item.img, "post")} alt={`팔로우한 작가 상품 이미지 ${index}`} />
-            </Link>
-          ))
-          .slice(0, 4)}
+        {render.length === 0 ? (
+          <FollowingNull>
+            <img src={cartNullIcon} alt="장바구니 아이콘" />
+            <p>아직 팔로잉한 유저가 없어요!</p>
+          </FollowingNull>
+        ) : (
+          <>
+            {render
+              .filter((i) => i !== undefined)
+              .map((item, index) => (
+                <Link key={item.id} to={`/productDetail/${item.id}`}>
+                  <img src={checkImageUrl(item.img, "post")} alt={`팔로우한 작가 상품 이미지 ${index}`} />
+                </Link>
+              ))
+              .slice(0, 4)}
+          </>
+        )}
       </article>
     </MainLeftSection>
   );
 }
 
 const MainLeftSection = styled.section`
+  article {
+    position: relative;
+    min-height: 90rem;
+  }
+
   h2 {
     margin-bottom: 16px;
   }
 
   img {
     width: 100%;
+  }
+`;
+
+const FollowingNull = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  height: 40rem;
+
+  position: sticky;
+  top: 5rem;
+
+  & > img {
+    width: 54px;
+    margin-bottom: 18px;
+  }
+
+  & > p {
+    font-family: var(--font--Regular);
+    color: var(--gray400-color);
+    margin-bottom: 32px;
   }
 `;
