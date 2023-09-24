60976eef3fa28fe579f177475589b803eb0d2550
Oh Ga Eun <dhrkdms4113@naver.com>
2023-09-25 00:34:42 +0900

    refactor: 디테일 페이지 타입 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -17,12 +17,13 @@ import { productGetUpdateAPI } from "../api";
 
 import { checkImageUrl } from "../utils";
 import TotalCountPrice from "../components/Detail/TotalCountPrice";
+import { product_I } from "../interface/product_I";
 
 export default function Detail() {
   const { id } = useParams();
 
   const [loading, setLoading] = useState(true);
-  const [productData, setProductData] = useState("");
+  const [productData, setProductData] = useState<product_I | null>(null);
   const [price, setPrice] = useState(0);
 
   useEffect(() => {
@@ -46,42 +47,44 @@ export default function Detail() {
         <DetailSkeleton />
       ) : (
         <>
-          <DetailWrap>
-            <DetailImage img={productData.itemImage.split(",")} />
+          {productData && (
+            <DetailWrap>
+              <DetailImage img={productData.itemImage.split(",")} />
 
-            <ProductDetail>
-              <div className="product_detail_top">
-                <ProfileUI
-                  key={productData.author._id}
-                  user_profile={checkImageUrl(productData.author.image, "profile")}
-                  user_name={productData.author.username}
-                  account_name={productData.author.accountname}
-                />
-              </div>
+              <ProductDetail>
+                <div className="product_detail_top">
+                  <ProfileUI
+                    key={productData.author._id}
+                    user_profile={checkImageUrl(productData.author.image, "profile")}
+                    user_name={productData.author.username}
+                    account_name={productData.author.accountname}
+                  />
+                </div>
 
-              <h2 className="product_title">{productData.itemName}</h2>
-              <p className="product_description">{productData.link}</p>
-              <DeliveryDescription>
-                <div className="delivery_date">
-                  <img src={DeliveryIcon} alt="박스 아이콘" />
-                  <h3 className="delivery_price_subtitle">배송 기간</h3>
-                  <p className="delivery_price_text">
-                    지금 주문하면 <strong>3일 이내</strong> 출고 예정 (주말, 공휴일 제외)
-                  </p>
-                </div>
-                <div className="delivery_price">
-                  <img src={MoneyIcon} alt="동전 아이콘" />
-                  <h3 className="delivery_price_subtitle">배송비</h3>
-                  <p className="delivery_price_text">
-                    구디 제품 80,000원 이상 구매시 무료배송
-                    <br />
-                    제주도를 포함한 도서/산간지역은 추가 배송비 3,000원
-                  </p>
-                </div>
-              </DeliveryDescription>
-              <TotalCountPrice productData={productData} />
-            </ProductDetail>
-          </DetailWrap>
+                <h2 className="product_title">{productData.itemName}</h2>
+                <p className="product_description">{productData.link}</p>
+                <DeliveryDescription>
+                  <div className="delivery_date">
+                    <img src={DeliveryIcon} alt="박스 아이콘" />
+                    <h3 className="delivery_price_subtitle">배송 기간</h3>
+                    <p className="delivery_price_text">
+                      지금 주문하면 <strong>3일 이내</strong> 출고 예정 (주말, 공휴일 제외)
+                    </p>
+                  </div>
+                  <div className="delivery_price">
+                    <img src={MoneyIcon} alt="동전 아이콘" />
+                    <h3 className="delivery_price_subtitle">배송비</h3>
+                    <p className="delivery_price_text">
+                      구디 제품 80,000원 이상 구매시 무료배송
+                      <br />
+                      제주도를 포함한 도서/산간지역은 추가 배송비 3,000원
+                    </p>
+                  </div>
+                </DeliveryDescription>
+                <TotalCountPrice productData={productData} />
+              </ProductDetail>
+            </DetailWrap>
+          )}
         </>
       )}
     </Layout>
