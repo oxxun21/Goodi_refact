eb523533a22c0442fb2432c2bb112aaf881d3374
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-17 15:29:44 +0900

    (#30) refactor: 채팅 폼 파일 이동

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,6 @@
 import Button from "./Button/Button";
 import ButtonFollow from "./Button/ButtonFollow";
 import ButtonPostLike from "./Button/ButtonPostLike";
-import Form from "./Form";
 import Input from "./Input";
 import LocalNav from "./LocalNav";
 import Modal from "./Modal";
@@ -9,4 +8,4 @@ import ScrollToTop from "./ScrollToTop";
 import Textarea from "./Textarea";
 import Toast from "./Toast";
 
-export { Button, ButtonFollow, ButtonPostLike, Form, Input, LocalNav, Modal, ScrollToTop, Textarea, Toast };
+export { Button, ButtonFollow, ButtonPostLike, Input, LocalNav, Modal, ScrollToTop, Textarea, Toast };
